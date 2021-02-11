a8ed244c8831f7c1497096da7953684d5a0a7e02
Hannah Sones <70013364+HannahSones@users.noreply.github.com>
2021-02-11 21:52:07 +0000

    Updating service worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,16 +2,82 @@
 
 console.log('This is your service-worker.js file!');
 
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
 const FILES_TO_CACHE = [
-    "/",
-    "/index.html",
-    "/styles.css",
-    "index.js",
-    "/manifest.webmanifest",
-    "database.js",
-    "icons/icon-192x192.png",
-    "icons/icon-512x512.png"
-  ];
-  
-  const CACHE_NAME = "static-cache-v2";
-  const DATA_CACHE_NAME = "data-cache-v1";
+  "/",
+  "/index.html",
+  "/styles.css",
+  "index.js",
+  "/manifest.webmanifest",
+  "database.js",
+  "icons/icon-192x192.png",
+  "icons/icon-512x512.png"
+];
+
+
+
+// install
+self.addEventListener('install', function (evt) {
+
+  evt.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log({ cache });
+        cache.addAll(FILES_TO_CACHE);
+      })
+      .catch((err) => {
+        console.log({ err });
+      })
+  );
+  // tell the browser to activate this service worker immediately once it
+  // has finished installing
+  self.skipWaiting();
+});
+// activate
+self.addEventListener('activate', function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log('Removing old cache data', key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+// fetch
+self.addEventListener('fetch', function (evt) {
+  if (evt.request.url.includes('/api/')) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+    return;
+  }
+  evt.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
+});
